9a5eaf2e90f6ccaf153b2a09f625f4054c16ddf1
guillo_e <lyoma.guillou@epitech.eu>
2014-01-27 19:41:26 +0100

    actions

---
+++ headers/Action.hpp
@@ -0,0 +1,41 @@
+#ifndef ACTION_H_
+# define ACTION_H_
+
+namespace	epil
+{
+  enum AType
+  {
+    NULL,
+    MODIFY,
+    COMPILE,
+    EXEC
+  };
+  
+  class	Action
+  {  
+    AType		_type;
+    bool		_state;
+    std::string		_action;
+
+  public:
+    Action();
+    Action(AType type);
+    Action(AType type, std::string action);
+    Action(Action &);
+    ~Action();
+
+    AType		getType() const;
+    bool		getState() const;
+    std::string		getAction() const;
+
+    void		setType(AType type);
+    void		setAction(std::string action);
+
+    bool		apply();
+
+    Action & operator=(Action &);
+  };
+
+};
+
+#endif /* !ACTION_H_ */
---
+++ src/Action.cpp
@@ -0,0 +1,39 @@
+#include "Action.hpp"
+
+using namespace epil;
+
+Action::Action() : _type(NIL), _state(false) {}
+Action::Action(AType type) : _type, _state(false) {}
+Action::Action(AType type, std::string action) : _type(type), _state(false), _action(action) {}
+Action::Action(Action &other) : _type(other._type), _state(other._state), _action(other._action) {}
+Action::~Action() {}
+
+AType		Action::getType() const { return _type; }
+bool		Action::getSet() const { return _state; }
+std::string	Action::getAction() const { return _action; }
+
+void		Action::setType(e_AType type) { _type = type; }
+void		Action::setAction(std::string action { _action = action; }
+
+bool		Action::apply()
+{
+  switch _type
+    {
+    case NIL:
+      return false;
+    case MODIFY:
+
+    case COMPILE:
+
+    case EXEC:
+
+    }
+}
+
+Action &	Action::operator=(Action &other)
+{
+  _type = other._type;
+  _state = other._state;
+  _action = other._action;
+  return *this;
+}
