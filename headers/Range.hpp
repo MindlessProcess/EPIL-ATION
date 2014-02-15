4686366d9f36a88bb3a147c0738a327586066556
guillo_e <lyoma.guillou@epitech.eu>
2014-02-15 14:57:28 +0100

    Range functionnal

--- headers/Range.hpp
+++ headers/Range.hpp
@@ -1,28 +1,28 @@
 #ifndef RANGE_HPP_
 # define RANGE_HPP_
 
+# include <list>
 # include <utility>
-
 
 namespace	epil
 {
 
   class		Range
   {
-    std::size_t						_anti;
-    std::list<std::pair<std::size_t, std::size_t> >	_list;
+    int					_anti;
+    std::list<std::pair<int, int> >	_list;
 
   public:
-    Range(std::size_t);
-    Range(std::pair<std::size_t, std::size_t>);
-    Range(std::pair<std::size_t, std::size_t>, std::size_t);
-    Range(std::pair<std::size_t, std::size_t>, std::pair<std::size_t, std::size_t>);
-    Range(std::list<std::pair<std::size_t, std::size_t> >);
-    Range(std::list<std::pair<std::size_t, std::size_t> >, std::size_t);
-    Range(std::list<std::pair<std::size_t, std::size_t> >, std::pair<std::size_t, std::size_t>);
-    Range(std::list<std::pair<std::size_t, std::size_t> >, std::list<std::pair<std::size_t, std::size_t> >);
+    Range(int anti = -1);
+    Range(std::pair<int, int> pair, int anti = -1);
+    Range(std::pair<int, int> pair, std::pair<int, int> anti_pair, int anti = -1);
+    Range(std::list<std::pair<int, int> > list, int anti = -1);
+    Range(std::list<std::pair<int, int> > list, std::pair<int, int> anti_pair, int anti = -1);
+    Range(std::list<std::pair<int, int> > list, std::list<std::pair<int, int> > alist, int anti = -1);
     ~Range();
-  
+
+    int						getAnti() const;
+    std::list<std::pair<int, int> >		getList() const;
   };
 
 };
--- sources/Range.cpp
+++ sources/Range.cpp
@@ -1,64 +1,86 @@
 #include "Range.hpp"
 
-using namespace	epil
+#include <list>
+#include <utility>
 
-yRange::Range(std::size_t anti)
+using namespace	epil;
+
+Range::Range(int anti)
 {
   _anti = (0 > anti) ? -1 : anti;
-  _list = std::list<std::pair<std::size_t, std::size_t> >(std::make_pair(-1, -1));
 }
 
-Range::Range(std::pair<std::size_t, std::size_t> pair)
+// Range::Range(std::pair<int, int> pair)
+// {
+//   _anti = -1;
+//   if (0 <= std::get<0>(pair) && 0 <= std::get<1>(pair))
+//     {
+//       if (std::get<0>(pair) > std::get<1>(pair))
+// 	_list.push_back(std::make_pair(std::get<1>(pair), std::get<0>(pair)));
+//       else
+// 	_list.push_back(pair);
+//     }
+// }
+
+Range::Range(std::pair<int, int> pair, int anti)
 {
-  _anti = -1;
-  _list = std::list<std::pair<std::size_t, std::size_t> >(std::make_pair(-1, -1));
+  _anti = (0 > anti) ? -1 : anti;
   if (0 <= std::get<0>(pair) && 0 <= std::get<1>(pair))
     {
       if (std::get<0>(pair) > std::get<1>(pair))
-	_list = std::list<std::pair<std::size_t, std::size_t> >(std::make_pair(std::get<1>(pair), std::get<0>(pair)));
+	_list.push_back(std::make_pair(std::get<1>(pair), std::get<0>(pair)));
       else
-	_list = std::list<std::pair<std::size_t, std::size_t> >(pair);
+	_list.push_back(pair);
     }
 }
 
-Range::Range(std::pair<std::size_t, std::size_t> pair, std::size_t anti)
+Range::Range(std::pair<int, int> pair, std::pair<int, int> anti_pair, int anti)
 {
   _anti = (0 > anti) ? -1 : anti;
-  _list = std::list<std::pair<std::size_t, std::size_t> >(std::make_pair(-1, -1));
-  if (0 <= std::get<0>(pair) && 0 <= std::get<1>(pair))
-    {
-      if (std::get<0>(pair) > std::get<1>(pair))
-	_list = std::list<std::pair<std::size_t, std::size_t> >(std::make_pair(std::get<1>(pair), std::get<0>(pair)));
-      else
-	_list = std::list<std::pair<std::size_t, std::size_t> >(pair);
-    }
-}
-
-Range::Range(std::pair<std::size_t, std::size_t> pair, std::pair<std::size_t, std::size_t> anti_pair)
-{
-  _anti = -1;
   if (0 <= std::get<0>(pair) && 0 <= std::get<1>(pair) && 0 <= std::get<0>(anti_pair) && 0 <= std::get<1>(anti_pair))
     {
-      std::pair<std::size_t, std::size_t>	t_pair;
-      std::pair<std::size_t, std::size_t>	t_anti_pair;
+      std::pair<int, int>	t_pair;
+      std::pair<int, int>	t_anti_pair;
       t_pair = (std::get<0>(pair) > std::get<1>(pair)) ? std::make_pair(std::get<1>(pair), std::get<0>(pair)) : pair;
       t_anti_pair = (std::get<0>(anti_pair) > std::get<1>(anti_pair)) ? std::make_pair(std::get<1>(pair), std::get<0>(anti_pair)) : anti_pair;
       if (std::get<0>(t_pair) < std::get<0>(t_anti_pair))
 	_list.push_back(std::make_pair(std::get<0>(t_pair), std::get<0>(t_anti_pair)));
       if (std::get<1>(t_pair) > std::get<1>(t_anti_pair))
-	_list.push_back(std::make_pair(std::get<1>(t_pair), std::get<1>(t_anti_pair)));
+	_list.push_back(std::make_pair(std::get<1>(t_anti_pair), std::get<1>(t_pair)));
     }
-  if (_list.empty())
-    _list = std::list<std::pair<std::size_t, std::size_t> >(std::make_pair(-1, -1));
 }
 
-Range::Range(std::list<std::pair<std::size_t, std::size_t> > list)
-{
-  std::list<std::pair<std::size_t, std::size_t> >		tl;
-  std::list<std::pair<std::size_t, std::size_t> >::iterator	it;
-  std::pair<std::size_t, std::size_t>				tp;
+// Range::Range(std::list<std::pair<int, int> > list)
+// {
+//   std::list<std::pair<int, int> >		tl;
+//   std::list<std::pair<int, int> >::iterator	it;
+//   std::pair<int, int>				tp;
 
-  _anti = -1;
+//   _anti = (0 > anti) ? -1 : anti;
+//   if (!list.empty())
+//     {
+//       for (it = list.begin(); it != list.end(); ++it)
+// 	{
+// 	  tp = *it;
+// 	  if (0 <= std::get<0>(tp) && 0 <= std::get<1>(tp))
+// 	    {
+// 	      if (std::get<0>(tp) > std::get<1>(tp))
+// 		tl.push_back(std::make_pair(std::get<1>(tp), std::get<0>(tp)));
+// 	      else
+// 		tl.push_back(tp);
+// 	    }
+// 	}
+//       _list = tl;
+//     }
+// }
+
+Range::Range(std::list<std::pair<int, int> > list, int anti)
+{
+  std::list<std::pair<int, int> >		tl;
+  std::list<std::pair<int, int> >::iterator	it;
+  std::pair<int, int>				tp;
+
+  _anti = (0 > anti) ? -1 : anti;
   if (!list.empty())
     {
       for (it = list.begin(); it != list.end(); ++it)
@@ -74,44 +96,16 @@ Range::Range(std::list<std::pair<std::size_t, std::size_t> > list)
 	}
       _list = tl;
     }
-  else
-    _list = std::list<std::pair<std::size_t, std::size_t> >(std::make_pair(-1, -1));
 }
 
-Range::Range(std::list<std::pair<std::size_t, std::size_t> > list, std::size_t anti)
+Range::Range(std::list<std::pair<int, int> > list, std::pair<int, int> anti_pair, int anti)
 {
-  std::list<std::pair<std::size_t, std::size_t> >		tl;
-  std::list<std::pair<std::size_t, std::size_t> >::iterator	it;
-  std::pair<std::size_t, std::size_t>				tp;
+  std::list<std::pair<int, int> >		tl;
+  std::list<std::pair<int, int> >::iterator	it;
+  std::pair<int, int>				tp;
+  std::pair<int, int>				ta;
 
   _anti = (0 > anti) ? -1 : anti;
-  if (!list.empty())
-    {
-      for (it = list.begin(); it != list.end(); ++it)
-	{
-	  tp = *it;
-	  if (0 <= std::get<0>(tp) && 0 <= std::get<1>(tp))
-	    {
-	      if (std::get<0>(tp) > std::get<1>(tp))
-		tl.push_back(std::make_pair(std::get<1>(tp), std::get<0>(tp)));
-	      else
-		tl.push_back(tp);
-	    }
-	}
-      _list = list;
-    }
-  else
-    _list = std::list<std::pair<std::size_t, std::size_t> >(std::make_pair(-1, -1));
-}
-
-Range::Range(std::list<std::pair<std::size_t, std::size_t> > list, std::pair<std::size_t, std::size_t> anti_pair)
-{
-  std::list<std::pair<std::size_t, std::size_t> >		tl;
-  std::list<std::pair<std::size_t, std::size_t> >::iterator	it;
-  std::pair<std::size_t, std::size_t>				tp;
-  std::pair<std::size_t, std::size_t>				ta;
-
-  _anti = -1;
   if (!list.empty())
     {
       ta = std::make_pair(-1, -1);
@@ -123,11 +117,11 @@ Range::Range(std::list<std::pair<std::size_t, std::size_t> > list, std::pair<std
 	  if (0 <= std::get<0>(tp) && 0 <= std::get<1>(tp))
 	    {
 	      tp = (std::get<0>(tp) > std::get<1>(tp)) ? std::make_pair(std::get<1>(tp), std::get<0>(tp)) : tp;
-	      if (std::get<1>(ta) >= std::get<1>(tp) || std::get<0>(ta) <= std::get<0>(tp))
+	      if (std::get<0>(ta) < std::get<1>(tp) && std::get<1>(ta) > std::get<0>(tp))
 		{
-		  if (std::get<0>(tp) < std::get<0>(ta))
+		  if (std::get<0>(ta) > std::get<0>(tp))
 		    tl.push_back(std::make_pair(std::get<0>(tp), std::get<0>(ta)));
-		  if (std::get<1>(tp) > std::get<1>(ta))
+		  if (std::get<1>(ta) < std::get<1>(tp))
 		    tl.push_back(std::make_pair(std::get<1>(ta), std::get<1>(tp)));
 		}
 	      else
@@ -136,11 +130,59 @@ Range::Range(std::list<std::pair<std::size_t, std::size_t> > list, std::pair<std
 	}
       _list = tl;
     }
-  else
-    _list = std::list<std::pair<std::size_t, std::size_t> >(std::make_pair(-1, -1));
 }
 
-Range::Range(std::list<std::pair<std::size_t, std::size_t> > list, std::list<std::pair<std::size_t, std::size_t> > alist)
+Range::Range(std::list<std::pair<int, int> > list, std::list<std::pair<int, int> > alist, int anti)
 {
-y
+  std::list<std::pair<int, int> >		tl;
+  std::list<std::pair<int, int> >::iterator	it;
+  std::pair<int, int>				ta;
+  std::pair<int, int>				tp;
+  bool							flag = false;
+
+  _anti = (0 > anti) ? -1 : anti;
+  if (!list.empty())
+    {
+      for (it = list.begin(); it != list.end(); ++it)
+	{
+	  if (!alist.empty() && flag == false)
+	    {
+	      ta = alist.front();
+	      ta = (std::get<0>(ta) > std::get<1>(ta)) ? std::make_pair(std::get<1>(ta), std::get<0>(ta)) : ta;
+	      alist.pop_front();
+	      flag = true;
+	      it = list.begin();
+	    }
+	  tp = *it;
+	  if (0 <= std::get<0>(tp) && 0 <= std::get<1>(tp))
+	    {
+	      tp = (std::get<0>(tp) > std::get<1>(tp)) ? std::make_pair(std::get<1>(tp), std::get<0>(tp)) : tp;
+	      if (false != flag && std::get<0>(ta) < std::get<1>(tp) && std::get<1>(ta) > std::get<0>(tp))
+		{
+		  it = list.erase(it);
+		  it = list.begin();
+		  if (std::get<0>(ta) > std::get<0>(tp))
+		    tl.push_back(std::make_pair(std::get<0>(tp), std::get<0>(ta)));
+		  if (std::get<1>(ta) < std::get<1>(tp))
+		    tl.push_back(std::make_pair(std::get<1>(ta), std::get<1>(tp)));
+		  flag = false;
+		}
+	      else if (false == flag)
+		tl.push_back(tp);
+	    }
+	}
+      _list = tl;
+    }
+}
+
+Range::~Range() {}
+
+int	Range::getAnti() const
+{
+  return _anti;
+}
+
+std::list<std::pair<int, int> >	Range::getList() const
+{
+  return _list;
 }
