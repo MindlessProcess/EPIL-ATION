a87ecb272c875f85030a25e68323b57fe5eaa2b2
Lyoma <lyoma.guillou@epitech.eu>
2014-03-02 18:13:52 +0100

    temporary work

--- headers/System/Action.hh
+++ headers/System/Action.hh
@@ -5,47 +5,78 @@
 // Login   <lucas@epitech.net>
 // 
 // Started on  Mon Feb 24 23:33:51 2014 Lucas Merlette
-// Last update Mon Feb 24 23:35:51 2014 Lucas Merlette
+// Last update Sun Mar  2 18:10:11 2014 Lyoma Guillou
 //
 
 #ifndef		EPIL_SYSTEM_ACTION_HH_
 # define	EPIL_SYSTEM_ACTION_HH_
 
+# include	<list>
+# include	<utility>
+# include	<iostream>
+# include	<fstream>
+
+# include	"BlockList.hpp"
+
 namespace	epil
 {
   enum		AType
-  {
-    NULL,
-    MODIFY,
-    COMPILE,
-    EXEC
-  };
-  
-  class		Action
-  {  
-    AType	_type;
-    bool	_state;
-    bool	(*_fnc)();
-    std::string	_action;
+    {
+      NULL,
+      MODIFY,
+      COMPILE,
+      EXEC
+    };
 
-    Action();
+  class		Action
+  {
   public:
     Action(AType type);
-    Action(AType type, std::string action);
-    Action(Action &);
+
+  private:
+    AType	_type;
+    Action	*_action;
+
+    Action();
     ~Action();
-
-    AType	getType() const;
-    bool	getState() const;
-    std::string	getAction() const;
-
-    void	setType(AType type);
-    void	setAction(std::string action);
-
-    bool	apply();
-
-    Action	&operator=(Action &);
   };
+
+  class		Write : public Action
+  {
+  public:
+    Write(std::string src, std::string dst, BlockList srcBlock, BlockList dstBlock);
+  private:
+    std::ifstream	_src;
+    std::ofstream	_dst;
+    BlockList		_srcBlock;
+    BlockList		_dstBlock;
+
+    Write();
+    ~Write();
+  };
+
+  class		Compile : public Action
+  {
+  public:
+  private:
+    
+
+    Compile();
+    ~Compile();
+  };
+
+  class		Exec : public Action
+  {
+  public:
+  private:
+    std::string		_cmd;
+    std::string		_opt;
+
+    Exec();
+    ~Exec();
+
+  };
+
 };
 
 #endif		/* !EPIL_SYSTEM_ACTION_HH_ */
