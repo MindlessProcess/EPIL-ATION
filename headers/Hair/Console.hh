3cc5be6a43281a074c0df546206f3e9dca7bd891
Lucas Merlette <lucas@lucas-HP-EliteBook-8540p.(none)>
2014-03-02 18:08:30 +0100

    Hair added

---
+++ headers/Hair/Console.hh
@@ -0,0 +1,24 @@
+//
+// Console.hh for Epil in /home/lucas/Documents/Epitech/Tech3/PFA/epil-ation/headers/UI
+// 
+// Made by Lucas Merlette
+// Login   <lucas@epitech.net>
+// 
+// Started on  Sun Mar  2 14:51:43 2014 Lucas Merlette
+// Last update Sun Mar  2 14:52:39 2014 Lucas Merlette
+//
+
+#ifndef	EPIL_UI_CONSOLE_HH_
+# define EPIL_UI_CONSOLE_HH_
+
+class Console
+{
+public:
+  Console();
+  ~Console();
+
+  void	
+private:
+};
+
+#endif /* !EPIL_UI_CONSOLE_HH_ */
