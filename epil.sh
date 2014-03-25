a3b1a87b5dd353e9f4a738177bcffee71040c616
Lyoma <lyoma.guillou@epitech.eu>
2014-03-25 10:01:01 +0100

    script to compile

--- epil.sh
+++ epil.sh
@@ -12,11 +12,13 @@ echo "Creating backup for main.cpp"
 mkdir -p backup
 cp src/main/main.cpp backup/
 
+echo "Copying the backup into src/main"
+cp backup/main.cpp src/main/main.cpp
+echo "Compiling Lucas' demo project for EPIL-ATION"
+g++ -std=c++0x -o lucas_app apps/lucas_app/main.cpp lib/epil.a
+
+echo "Copying the backup into src/main"
+cp backup/main.cpp src/main/main.cpp
 echo "Compiling Lyoma's demo project for EPIL-ATION"
 g++ -std=c++0x -o lyoma_app apps/lyoma_app/main.cpp lib/epil.a
 
-echo "Compiling Lucas' demo project for EPIL-ATION"
-#
-# FIXME FIXME FIXME FIXME FIXME
-#
-#g++ -std=c++0x -o lucas_app apps/lucas_app/ [FILES NAME HERE] lib/epil.a
